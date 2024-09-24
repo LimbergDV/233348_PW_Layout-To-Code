8ea708d04ed97f558f4b9b87c14c51396562ce34
LimbergDV <limbergdelvalle@gmail.com>
2024-09-24 14:04:52 -0600

    modified: rutas para cada página (enrutamiento)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { PrincipalViewComponent } from './principal-view/principal-view.component';
+import { StatsViewComponent } from './stats-view/stats-view.component';
+import { FootballPlayersViewComponent } from './football-players-view/football-players-view.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: NavBarComponent },
-  { path: 'about', component: NavBarComponent },
-  { path: 'contact', component: NavBarComponent },
+  { path: '', redirectTo: '/Inicio', pathMatch: 'full' },
+  { path: 'Inicio', component: PrincipalViewComponent },
+  { path: 'Estadisticas', component:  StatsViewComponent},
+  { path: 'Futbolistas', component: FootballPlayersViewComponent },
+  { path: 'Partidos', component: FootballPlayersViewComponent },
 ];
 
 @NgModule({
